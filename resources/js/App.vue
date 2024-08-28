08ceca72cbe7def72d453ad12a2e0216ea093839
Rares Petrescu <rar3sh.p@gmail.com>
2024-08-28 19:46:59 +0300

    First commit - Vue + Laravel installed and running smooth

---
+++ resources/js/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>
+        Main app vue. Here it will be a NICE table
+    </h1>
+
+    <div class="row">
+        <div class="col">aaa</div>
+        <div class="col">bbb</div>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<head>
+    @vite(['resources/js/app.js'])
+    <!-- Bootstrap -->
+    <link rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous"
+    >
+</head>
+
+<body>
+    <h3>ORDER DEMO APP made by Rares for Tradecentric :)</h3>
+    <div id="order_app"></div>
+
+    <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@3.4.1/dist/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
+</body>
