700ddca9f4b86c8609178ce0b5d1eff79fdecbe5
Rares Petrescu <rar3sh.p@gmail.com>
2024-08-29 16:50:01 +0300

    Added Order Model

---
+++ app/Models/Order.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\OrderFactory;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+/**
+ * @method static OrderFactory factory(...$parameters)
+ */
+class Order extends Model
+{
+    use SoftDeletes;
+    use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        'order_number',
+        'buyer_name',
+        'total',
+    ];
+}
